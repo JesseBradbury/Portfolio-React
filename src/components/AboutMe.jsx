413a72c1098596c2101a33ed71da9ecfdc1017a8
JB <bradburyjesse7@gmail.com>
2024-03-21 14:29:21 -0700

    Added ' fix in About Me.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -7,15 +7,15 @@ export default function AboutMe() {
                 Bootcamp.<br></br>
 
                 I am an experienced teacher and technical support advisor, dedicated mentor, and passionate life long learner.
-                Currently enriching lives with Apple Retail as a Genius, I've always been drawn to helping others use
+                Currently enriching lives with Apple Retail as a Genius, I&#39;ve always been drawn to helping others use
                 innovative and exciting new technology. Recently cultivated experiences include training and employee
                 development, eLearning and instructional design, retail floor leadership, and technical hardware repair
                 training.<br></br>
 
                 Before working in technical support I was a high school teacher where I taught computer science and
-                business courses in Mesa Arizona. I've also taught drumline and marching band for 10+ years with
+                business courses in Mesa Arizona. I&#39;ve also taught drumline and marching band for 10+ years with
                 multiple programs across Arizona. Music and marching band taught me many valuable lessons while I was a
-                student, I'm proud to be able to give back to my community and provide those memorable and positive
+                student, I&#39;m proud to be able to give back to my community and provide those memorable and positive
                 experiences for young musicians.<br></br>
 
                 Outside of work you will find me hiking or exploring the outdoors with my family, gardening, playing a

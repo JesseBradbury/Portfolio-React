2b6d5ca9b9292a431c8d28fe9518e89a339bb8e6
JB <bradburyjesse7@gmail.com>
2024-03-22 00:39:00 -0700

    Updated Contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,45 @@
+// WHEN I am presented with the Contact section
+// THEN I see a contact form with fields for a name, an email address, and a message
+// WHEN I move my cursor out of one of the form fields without entering text
+// THEN I receive a notification that this field is required
+// WHEN I enter text into the email address field
+// THEN I receive a notification if I have entered an invalid email address
+
+
+import { TextField, Container } from "@mui/material"
+
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import { Paper } from "@mui/material";
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+
+
 export default function Contact() {
     return (
-        <div>CONTACT ROUTE
-            
+        <div>
+                <Paper>
+            <Container>
+
+                    <div>
+                        <h2>Contact Me</h2>
+                    </div>
+                    <Grid container spacing={2}>
+                        <Grid xs={12} sm={5}>
+                            <TextField required id="outlined-basic" label="Name" variant="outlined" />
+                        </Grid>
+                        <Grid xs={12} sm={6}>
+                            <TextField required id="outlined-basic" label="e-mail" variant="outlined" />
+                        </Grid>
+                    </Grid>
+                    <Grid>
+                        <TextField fullWidth label="Message" id="message outlined-multiline-flexible " multiline maxRows={4} margin="normal" />
+                    </Grid>
+                    <Button variant="contained" endIcon={<SendIcon />}>
+        Send
+      </Button>
+            </Container>
+                </Paper>
+
 
         </div>
     )
